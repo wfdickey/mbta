ab240f10b5fca99340e9d3a0643c88527d5cf2e5
Will Dickey <wfdickey@gmail.com>
2014-01-17 13:48:20 -0500

    debuggin

--- js/script.js
+++ js/script.js
@@ -38,6 +38,40 @@ $("#small-screen-selector dd").off().on('click', function(event) {
 
 });
 
+$("#searchInput").keyup(function () {
+    //split the current value of searchInput
+    var data = this.value.split(" ");
+    //create a jquery object of the rows
+    var jo = $("#fbody").find("tr");
+    if (this.value == "") {
+        jo.show();
+        return;
+    }
+    //hide all the rows
+    jo.hide();
+
+    //Recusively filter the jquery object to get results.
+    jo.filter(function (i, v) {
+        var $t = $(this);
+        for (var d = 0; d < data.length; ++d) {
+            if ($t.is(":contains('" + data[d] + "')")) {
+                return true;
+            }
+        }
+        return false;
+    })
+    //show the rows that match.
+    .show();
+}).focus(function () {
+    this.value = "";
+    $(this).css({
+        "color": "black"
+    });
+    $(this).unbind('focus');
+}).css({
+    "color": "#C0C0C0"
+});
+
 
 jQuery(function ($) {
     // Asynchronously Load the map API 
